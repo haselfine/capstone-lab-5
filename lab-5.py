65d0d6b6d432f7fcc930b043e39ba65ce67b6661
Ben <benhaseltine@gmail.com>
2020-09-22 18:41:37 -0500

    added add_record_holder function

--- lab-5.py
+++ lab-5.py
@@ -2,9 +2,6 @@ from peewee import *
 
 db = SqliteDatabase('record_holder.sqlite')
 
-class InputError(Exception):
-    pass
-
 class Record_Holder(Model):
     name = CharField()
     country = CharField()
@@ -16,8 +13,8 @@ class Record_Holder(Model):
     def __str__(self):
         return f'{self.name} from {self.country} holds a record of {self.catches} catches'
 
-def add_record_holder(self):
-    try:
-        new_rh_name = input('Enter the name of the record holder you would like to add: ')
-        
-    except InputError():
+def add_record_holder():
+    new_rh_name = input('Enter the name of the record holder you would like to add: ')
+    new_rh_country = input('Enter the home country of the record holder: ')
+    new_rh_catches = int(input('Enter top number of catches by the record holder: '))
+    return Record_Holder(name=new_rh_name, country=new_rh_country, catches=new_rh_catches)
